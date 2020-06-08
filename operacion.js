b57dcfd58878cb0a214a7339d215857314ca18b6
delavega <201421099@udh.edu.pe>
2020-06-08 09:17:09 -0500

    primera pregunta

---
+++ operacion.js
@@ -0,0 +1,8 @@
+var x, y;
+x = 2;
+y = 30;
+z = x + y;
+document.getElementById('suma').innerHTML = "la suma es: "+z;
+document.getElementById('resta').innerHTML = "la resta es: "+(y-x);
+document.getElementById('mutiplicacion').innerHTML = "la multiplicacion es: "+(x*y);
+document.getElementById('la division es: ').innerHTML = "la div es: "+(y/x);
